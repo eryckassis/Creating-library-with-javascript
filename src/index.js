336531d33958ffbf15f7bf442e01796d7089570c
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-12 22:35:27 -0300

    Implement file reading functionality and log file content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
-console.log(link);
+
+fs.readFile(link, "uft-8", (erro, texto) => {
+  console.log(texto);
+});
