3b26e033f108e180d1cfbedc8018f239d6ce5ff3
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-12 22:59:39 -0300

    Fix string quotes and correct encoding type in file reading function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
+const link = caminhoArquivo[2]; 
 
-fs.readFile(link, "uft-8", (erro, texto) => {
+fs.readFile(link, 'utf-8', (erro, texto) => {
   console.log(texto);
-});
+})
