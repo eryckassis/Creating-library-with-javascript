6b9e70dcf8230fce7f9891c611929dc848507279
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-18 22:32:25 -0300

    Refactor trataErros function to handle specific error types and return detailed messages

--- src/erros/funcoesErro.js
+++ src/erros/funcoesErro.js
@@ -7,14 +7,3 @@ function trataErros(erro) {
 }
 
 module.exports = trataErros;
-function trataErros(erro) {
-  if (erro.code === "ENOENT") {
-    return "Arquivo não encontrado";
-  } else if (erro instanceof TypeError) {
-    return "Erro de tipo: " + erro.message;
-  } else if (erro instanceof ReferenceError) {
-    return "Erro de referência: " + erro.message;
-  } else {
-    return "Erro desconhecido: " + erro.message;
-  }
-}
