e1de8cc0aeacc00e6bcc4effcbf36db381dece9c
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-19 23:42:48 -0300

    Refactor file reading to save word count results to a new file

--- src/cli.js
+++ src/cli.js
@@ -7,12 +7,25 @@ const trataErros = require("./erros/funcoesErro");
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, "utf8", (erro, texto) => {
   if (erro) throw erro;
   try {
-    contaPalavras(texto);
+    const resultado = contaPalavras(texto);
+    criaESalvaArquivo(resultado, endereco);
   } catch (erro) {
     trataErros(erro);
   }
 });
+
+async function criaESalvaArquivo(listaPalavras, edereco) {
+  const arquivoNovo = `${edereco}/resultado.txt`;
+  const textoPalavras = JSON.stringify(listaPalavras);
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("Arquivo criado com sucesso");
+  } catch (erro) {
+    throw erro;
+  }
+}
