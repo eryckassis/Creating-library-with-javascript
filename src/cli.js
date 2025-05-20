86c5d6b17fd8362d96530ad4e60ac050d430fe6d
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-19 23:28:26 -0300

    Add package.json for project and src directory; initialize with basic configuration

---
+++ src/cli.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const trataErros = require("./erros/funcoesErro");
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+
+fs.readFile(link, "utf8", (erro, texto) => {
+  if (erro) throw erro;
+  try {
+    contaPalavras(texto);
+  } catch (erro) {
+    trataErros(erro);
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 const fs = require("fs");
 const trataErros = require("./erros/funcoesErro");
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-
-fs.readFile(link, "utf8", (erro, texto) => {
-  if (erro) throw erro;
-  try {
-    contaPalavras(texto);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
-
 function contaPalavras(texto) {
   const paragrafos = extraiParagrafos(texto);
   const contagem = paragrafos.flatMap((paragrafo) => {
