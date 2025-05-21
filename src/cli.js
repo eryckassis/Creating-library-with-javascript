6fd18bea783aaf2f53b9965673cb9883bac91fe8
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-21 20:59:53 -0300

    Refactor CLI processing logic to improve error handling and streamline file reading; remove direct argument access in favor of command options

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import path from "path";
 import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
@@ -18,21 +19,31 @@ program
       program.help();
       return;
     }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log("Arquivo processado com sucesso");
+    } catch (erro) {
+      console.log("Erro ao processar arquivo", erro);
+    }
   });
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+program.parse();
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-  try {
-    if (erro) throw erro;
-    const resultato = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultato = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
